bf83dcff9eab39087e448635afd7b057b12bfabb
dudgp128 <20181048@sungshin.ac.kr>
2023-04-22 23:57:51 +0900

    [FEAT] 다크모드 설정하기

---
+++ frontend/src/DarkModeToggle.js
@@ -0,0 +1,37 @@
+class DarkModeToggle {
+  constructor({ $target }) {
+    const $wraaper = document.createElement("section");
+    const $darkModeToggle = document.createElement("input");
+    this.$darkModeToggle = $darkModeToggle;
+    this.$darkModeToggle.type = "checkbox";
+
+    $darkModeToggle.className = "darkModeToggle";
+
+    $wraaper.appendChild($darkModeToggle);
+    $target.appendChild($wraaper);
+
+    this.initColorMode();
+
+    $darkModeToggle.addEventListener("change", (e) => {
+      this.setColorMode(e.target.checked);
+    });
+  }
+
+  initColorMode() {
+    // checkbox 초기화
+    this.$darkModeToggle.checked = window.matchMedia(
+      "(prefers-color-schema:dark)"
+    ).matches;
+
+    // html 초기화
+    this.setColorMode(window.matchMedia("(prefers-color-schema:dark)").matches);
+  }
+
+  setColorMode(isDarkMode) {
+    document.documentElement.setAttribute(
+      "color-mode",
+      isDarkMode ? "dark" : "light"
+    );
+  }
+  render() {}
+}
