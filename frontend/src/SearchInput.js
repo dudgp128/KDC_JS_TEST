b2ebc1457215e08dab435b76a0c0788f8aa5c20d
dudgp128 <20181048@sungshin.ac.kr>
2023-04-22 23:53:52 +0900

    [STYLE] HTML 시멘틱 언어로 변경하기

--- frontend/src/SearchInput.js
+++ frontend/src/SearchInput.js
@@ -2,14 +2,17 @@ const TEMPLATE = '<input type="text">';
 
 class SearchInput {
   constructor({ $target, onSearch }) {
+    const $wraaper = document.createElement("section");
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
 
     $searchInput.className = "SearchInput";
-    $target.appendChild($searchInput);
 
-    $searchInput.addEventListener("keyup", e => {
+    $wraaper.appendChild($searchInput);
+    $target.appendChild($wraaper);
+
+    $searchInput.addEventListener("keyup", (e) => {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
       }
