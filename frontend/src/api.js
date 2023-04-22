13c77ca1f87cd0ad4ce17c935ce916edbaba0125
dudgp128 <20181048@sungshin.ac.kr>
2023-04-23 00:34:40 +0900

    [STYLE] async/await으로 변경하기

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,8 @@
 const API_ENDPOINT = "http://localhost:4001";
 
 const api = {
-  fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
-    );
+  fetchCats: async (keyword) => {
+    const res = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
+    return await res.json();
   },
 };
