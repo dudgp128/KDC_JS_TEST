4ca51c5abac10366ac0a14d12b2d8735524ad9b2
dudgp128 <20181048@sungshin.ac.kr>
2023-04-24 13:51:33 +0900

    [STYLED] 불필요한 await 제거하기

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,10 +3,10 @@ const API_ENDPOINT = "http://localhost:4001";
 const api = {
   fetchCats: async (keyword) => {
     const res = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
-    return await res.json();
+    return res.json();
   },
   fetchRandomCats: async () => {
     const res = await fetch(`${API_ENDPOINT}/api/cats/random50`);
-    return await res.json();
+    return res.json();
   },
 };
