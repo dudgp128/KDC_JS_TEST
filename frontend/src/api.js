b5e5db570302a8674e6259f96d6a62aa7f1a5811
dudgp128 <20181048@sungshin.ac.kr>
2023-04-30 14:37:44 +0900

    [FACTORY] 서버 요청 메소드(request)로 분리하기

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,22 +1,28 @@
 const API_ENDPOINT = "http://localhost:4001";
 
+const request = async (url) => {
+  try {
+    const result = await fetch(url);
+
+    if (result.status === 200) {
+      return result.json();
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+
 const api = {
   fetchCats: async (keyword) => {
-    const res = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
-    return res.json();
+    return request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
   },
   fetchCatsPage: async (keyword, page) => {
-    const res = await fetch(
-      `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}`
-    );
-    return res.json();
+    return request(`${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}`);
   },
   fetchRandomCats: async () => {
-    const res = await fetch(`${API_ENDPOINT}/api/cats/random50`);
-    return res.json();
+    return request(`${API_ENDPOINT}/api/cats/random50`);
   },
   fetchInfo: async (id) => {
-    const res = await fetch(`${API_ENDPOINT}/api/cats/${id}`);
-    return res.json();
+    return request(`${API_ENDPOINT}/api/cats/${id}`);
   },
 };
