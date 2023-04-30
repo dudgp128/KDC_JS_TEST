0f35946b630cb23d5490b57ea5bbae29b96eb7c3
dudgp128 <20181048@sungshin.ac.kr>
2023-04-30 14:40:13 +0900

    [FEAT] 서버 요청 에러시 알림창 띄우기

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,20 @@
 const API_ENDPOINT = "http://localhost:4001";
 
+const REQUEST_ERROR = {
+  500: { msg: "요청 실패" },
+};
+
 const request = async (url) => {
   try {
     const result = await fetch(url);
 
     if (result.status === 200) {
       return result.json();
+    } else {
+      throw REQUEST_ERROR[result.status];
     }
   } catch (error) {
-    alert(error);
+    alert(error.msg);
   }
 };
 
